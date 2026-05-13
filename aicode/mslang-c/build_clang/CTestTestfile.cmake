4b9d9f09dd482e6b4482ec77b911c4503df494f9
ASMloverS <asmlover@outlook.com>
2026-05-14 00:47:52 +0800

    🔧 chore(.gitignore): ignore build_clang and untrack its files

--- aicode/mslang-c/build_clang/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Workspace/Repositories/study/aicode/mslang-c
-# Build directory: C:/Workspace/Repositories/study/aicode/mslang-c/build_clang
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
